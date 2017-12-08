0b6cd5e6a9f6c8b759a9a877238777a73f58992c
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-12-08 18:30:11 -0500

    Add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,21 @@
+import {shallow} from 'enzyme';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
 import App from './App';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
+});
+
+it('has some children', () => {
+    const app = shallow(<App />);
+
+    expect(app.getElements().some(() => true)).toBe(true);
+});
+
+it('matches the snapshot', () => {
+    const app = shallow(<App />);
+    expect(app).toMatchSnapshot();
 });
