2ab0ae0110d0e7f4d49921b5a78c7eed59e925fa
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-12-10 15:18:09 -0500

    Remove index.css

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,5 @@ import {render} from 'react-snapshot';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import './index.css';
-
 render(<App />, document.getElementById('root') as HTMLElement);
 registerServiceWorker();
