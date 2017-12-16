2aa5d4071f1e06c868a92acc40b4b001b1eff2a5
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-12-16 00:12:59 -0500

    Simplify service

--- src/services/tags.ts
+++ src/services/tags.ts
@@ -2,11 +2,9 @@ import {Observable} from 'rxjs/Observable';
 import {ajax} from 'rxjs/observable/dom/ajax';
 import {map} from 'rxjs/operators';
 
-const source$: Observable<string[]> = ajax({
+export const tags$: Observable<string[]> = ajax({
     url: 'https://conduit.productionready.io/api/tags',
     async: true,
     method: 'GET',
     responseType: 'json',
 }).pipe(map(({response: {tags}}) => tags));
-
-export const tags$ = (): Observable<string[]> => source$;
