8d4e4ae3469d323af295a29358adb1c308d00991
Spencer Turkel <spencer@palmbeachsoftware.com>
2017-12-16 16:36:31 -0500

    Simplify and expand service tests

--- src/services/tags.test.ts
+++ src/services/tags.test.ts
@@ -1,20 +1,25 @@
-import {Observable} from 'rxjs/Observable';
 import {timer} from 'rxjs/observable/timer';
 import {first, mapTo} from 'rxjs/operators';
 import {TestScheduler} from 'rxjs/Rx';
 
 import {tags$ as tags$Factory} from './tags';
+import {never} from 'rxjs/observable/never';
 
 describe('tags$', () => {
+    it('should query https://conduit.productionready.io/api/tags', () => {
+        tags$Factory(url => {
+            expect(url).toBe('https://conduit.productionready.io/api/tags');
+            return never();
+        });
+    });
+
     it('should return tags from response', () => {
         const tags = ['hello', 'world'];
         const testScheduler = new TestScheduler((actual, expected) => expected(actual).toEqual(expected));
 
-        const ajax = jest.fn<Observable<{response: {tags: string[]}}>>(url =>
+        const tags$ = tags$Factory(() =>
             timer(testScheduler.createTime('--|'), testScheduler).pipe(first(), mapTo({response: {tags}})),
         );
-
-        const tags$ = tags$Factory(ajax);
 
         testScheduler.expectObservable(tags$).toBe('--r', {r: tags});
     });
